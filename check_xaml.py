78f4669a91021227d0325d2a87bd92daec1e0fc7
EMGit269 <2693302308@qq.com>
2026-05-03 10:24:38 +0800

    Remove unused helper scripts

--- check_xaml.py
+++
@@ -1,9 +0,0 @@
-with open('ADDGH/ChatWindow.cs', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-start = content.find('string xaml = @"')
-end = content.find('";\n            try', start)
-xaml = content[start+16:end]
-
-print('Double quotes count:', xaml.count('"'))
-print('Escaped quotes count:', xaml.count('""'))
--- extract_xaml.py
+++
@@ -1,9 +0,0 @@
-with open('ADDGH/ChatWindow.cs', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-start = content.find('string xaml = @"')
-end = content.find('";\n            try', start)
-xaml = content[start+16:end]
-
-with open('ADDGH/ui.xaml', 'w', encoding='utf-8') as f:
-    f.write(xaml.replace('""', '"'))
